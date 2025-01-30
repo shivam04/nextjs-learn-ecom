bcc6315d4b9aeca83dda3692ac804d7367dcd495
Shivam Sinha <shvmsnha@amazon.com>
2025-01-30 23:50:24 +0530

    Product Card

--- components/shared/product/product-list.tsx
+++ components/shared/product/product-list.tsx
@@ -1,3 +1,5 @@
+import ProductCard from "./product-card";
+
 const ProductList = ( {data, title, limit}: { data: any; title?: string; limit?:number; } ) => {
     const limitData = limit ? data.slice(0, limit) : data;
     return (
@@ -6,7 +8,7 @@ const ProductList = ( {data, title, limit}: { data: any; title?: string; limit?:
             { data.length > 0 ? (
                 <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4">
                     { limitData.map((product: any) => (
-                        <div key={product.id}>{product.name}</div>
+                        <ProductCard key={product.slug} product={product}/>
                     )) }
                 </div>
             ): (
