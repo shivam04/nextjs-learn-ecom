6cfe984e44ff3d0aa83c36ad31e4196749dcb137
Shivam Sinha <shvmsnha@amazon.com>
2025-01-30 22:13:54 +0530

    Responsive sheet menu

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,7 +1,3 @@
-export const metadata = {
-  title: "Home"
-}
-
 const HomePage = () => {
   return <div>ProStore</div>   
 }
--- components/footer.tsx
+++ components/footer.tsx
@@ -1,7 +1,18 @@
+"use client";
+
 import { APP_NAME } from "@/lib/constants";
+import { useEffect, useState } from "react";
 
 const Footer = () => {
+    const [mounted, setMounted] = useState(false);
     const currentYear = new Date().getFullYear();
+
+    useEffect(() => {
+        setMounted(true);
+    }, []);
+
+    if (!mounted) return null;
+
     return (
         <footer className="border-t">
             <div className="p-5 flex-center">
