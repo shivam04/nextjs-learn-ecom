42b7beef04d4c471a071dfbd4aeb6de9acad67ed
Shivam Sinha <shvmsnha@amazon.com>
2025-01-31 23:27:26 +0530

    signup form and page

--- lib/constants/index.ts
+++ lib/constants/index.ts
@@ -7,3 +7,10 @@ export const signInDefaultValues = {
     email: '',
     password: ''
 }
+
+export const signUpDefaultValues = {
+    email: '',
+    password: '',
+    name: '',
+    confirmPassword: ''
+}
