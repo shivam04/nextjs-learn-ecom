96224b78b497bafd5524ef8a7ae0920de3fd7e26
Shivam Sinha <shvmsnha@amazon.com>
2025-01-31 19:43:48 +0530

    Product type fix

--- types/index.ts
+++ types/index.ts
@@ -3,6 +3,7 @@ import { insertProductSchema } from "@/lib/validators";
 
 export type Product = z.infer<typeof insertProductSchema> & {
     id: string;
-    rating: number;
+    rating: string;
     createdAt: Date;
+    numReviews: number;
 }
