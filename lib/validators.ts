a2a9412ece8739d287c9fda17f880631fb252507
Shivam Sinha <shvmsnha@amazon.com>
2025-01-31 23:18:25 +0530

    signup zod schema and action

--- lib/validators.ts
+++ lib/validators.ts
@@ -10,11 +10,11 @@ const currency = z
 
 // Schema for inseting products
 export const insertProductSchema = z.object({
-    name: z.string().min(3, 'Name must be at least 2 characters'),
-    slug: z.string().min(3, 'Slug must be at least 2 characters'),
-    category: z.string().min(3, 'Category must be at least 2 characters'),
-    brand: z.string().min(3, 'Brand must be at least 2 characters'),
-    description: z.string().min(3, 'Description must be at least 2 characters'),
+    name: z.string().min(3, 'Name must be at least 3 characters'),
+    slug: z.string().min(3, 'Slug must be at least 3 characters'),
+    category: z.string().min(3, 'Category must be at least 3 characters'),
+    brand: z.string().min(3, 'Brand must be at least 3 characters'),
+    description: z.string().min(3, 'Description must be at least 3 characters'),
     stock: z.coerce.number(),
     images: z.array(z.string()).min(1, 'Product must have at least one image'),
     isFeatured: z.boolean(),
@@ -26,4 +26,14 @@ export const insertProductSchema = z.object({
 export const signInFormSchema = z.object({
     email: z.string().email('Invalid email address'),
     password: z.string().min(6, 'Password must be at least 6 characters')
+});
+
+export const signUpFormSchema = z.object({
+    name: z.string().min(3, 'Name must be at least 3 characters'),
+    email: z.string().email('Invalid email address'),
+    password: z.string().min(6, 'Password must be at least 6 characters'),
+    confirmPassword: z.string().min(6, 'Password must be at least 6 characters')
+}).refine((data) => data.password === data.confirmPassword, {
+    message: "Passwords don't match",
+    path: ['confirmPassword'],
 });
