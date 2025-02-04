3f7f69122b6111c3daadedf9793e646748b24a4b
Shivam Sinha <shvmsnha@amazon.com>
2025-02-04 16:12:04 +0530

    cart schema and migration

--- types/index.ts
+++ types/index.ts
@@ -1,5 +1,5 @@
 import { z } from "zod";
-import { insertProductSchema } from "@/lib/validators";
+import { insertProductSchema, insertCartSchema, cartItemSchema } from "@/lib/validators";
 
 export type Product = z.infer<typeof insertProductSchema> & {
     id: string;
@@ -7,3 +7,6 @@ export type Product = z.infer<typeof insertProductSchema> & {
     createdAt: Date;
     numReviews: number;
 }
+
+export type Cart = z.infer<typeof insertCartSchema>;
+export type CartItem = z.infer<typeof cartItemSchema>;
