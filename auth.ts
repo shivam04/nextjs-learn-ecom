7878b0831ed66a03d7e451f6e66842770e7d715b
Shivam Sinha <shvmsnha@amazon.com>
2025-02-04 18:05:59 +0530

    Session CartId Cookie

--- auth.ts
+++ auth.ts
@@ -1,9 +1,11 @@
+/* eslint-disable @typescript-eslint/no-unused-vars */
 import NextAuth from 'next-auth';
 import { PrismaAdapter } from '@auth/prisma-adapter';
 import { prisma } from './db/prisma';
 import CredentialsProvider from 'next-auth/providers/credentials'
 import { compareSync } from 'bcrypt-ts-edge';
 import type { NextAuthConfig } from 'next-auth';
+import { NextResponse } from 'next/server';
 
 export const config = {
     pages: {
@@ -68,7 +70,7 @@ export const config = {
 
             return session;
         },
-        // eslint-disable-next-line @typescript-eslint/no-unused-vars, @typescript-eslint/no-explicit-any
+        // eslint-disable-next-line @typescript-eslint/no-explicit-any
         async jwt({ token, user, trigger, session }: any) {
             // Asign user fields to token
             if (user) {
@@ -89,6 +91,31 @@ export const config = {
             //console.log("Token", token);
 
             return token;
+        },
+        // eslint-disable-next-line @typescript-eslint/no-explicit-any
+        authorized({ request, auth }: any) {
+            // Check for session cart cookie
+            if (!request.cookies.get('sessionCartId')) {
+                // Generate new session cart id cookie
+                const sessionCartId = crypto.randomUUID();
+                
+                // Clone request headers
+                const newRequestHeaders = new Headers(request.headers);
+
+                // Create new response and add to new headers
+                const response = NextResponse.next({
+                    request: {
+                        headers: newRequestHeaders
+                    }
+                });
+
+                // Set newly generated session cartId in the response cookies
+                response.cookies.set('sessionCartId', sessionCartId);
+
+                return response;
+            } else {
+                return true;
+            }
         }
     }
 } satisfies NextAuthConfig;
