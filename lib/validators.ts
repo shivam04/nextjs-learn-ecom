30ab578dcb90349d6353c462bf5ceb05e0932138
Shivam Sinha <shvmsnha@amazon.com>
2025-02-05 13:27:44 +0530

    Approve and update order

--- lib/validators.ts
+++ lib/validators.ts
@@ -101,3 +101,12 @@ export const insertOrderItemSchema = z.object({
     price: currency,
     qty: z.number(),
 });
+
+// Schema for the PayPal paymentResult
+export const paymentResultSchema = z.object({
+    id: z.string(),
+    status: z.string(),
+    email_address: z.string(),
+    pricePaid: z.string(),
+});
+  
--- types/index.ts
+++ types/index.ts
@@ -1,5 +1,5 @@
 import { z } from "zod";
-import { insertProductSchema, insertCartSchema, cartItemSchema, shippingAddressSchema, insertOrderSchema, insertOrderItemSchema } from "@/lib/validators";
+import { insertProductSchema, insertCartSchema, cartItemSchema, shippingAddressSchema, insertOrderSchema, insertOrderItemSchema, paymentResultSchema } from "@/lib/validators";
 
 export type Product = z.infer<typeof insertProductSchema> & {
     id: string;
@@ -22,3 +22,4 @@ export type Order = z.infer<typeof insertOrderSchema> & {
     orderitems: OrderItem[];
     user: { name: string; email: string};
 };
+export type PaymentResult = z.infer<typeof paymentResultSchema>;
