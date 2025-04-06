a41f3b3761517218010254859bc2b5da7497bb05
Shivam Sinha <shvmsnha@amazon.com>
2025-04-06 18:15:04 +0530

    Sorting Products

--- app/(root)/search/page.tsx
+++ app/(root)/search/page.tsx
@@ -33,6 +33,8 @@ const prices = [
 
 const ratings = [4, 3, 2, 1];
 
+const sortOrders = ['newest', 'lowest', 'highest', 'rating'];
+
 const SearchPage = async (props: {
     searchParams: Promise<{
         q?: string;
@@ -178,6 +180,18 @@ const SearchPage = async (props: {
                                 <Link href='/search'>Clear</Link>
                             </Button>
                             ) : null }
+                    </div>
+                    <div>
+                        Sort By{' '}
+                        { sortOrders.map((s) => (
+                            <Link
+                                key={s}
+                                href={getFilterUrl({ s })}
+                                className={`mx-2 ${sort === s && 'font-bold'}`}
+                            >
+                                {s}
+                            </Link>
+                        ))}
                     </div>
                 </div>
                 <div className="grid grid-cols-1 gap-4 md:grid-cols-3">
