b8b5c000803866e2375299e7857e796c9d0da971
Shivam Sinha <shvmsnha@amazon.com>
2025-04-08 15:22:38 +0530

    Read PEM From Secret Manager

--- app/api/genrate-signature/route.ts
+++ app/api/genrate-signature/route.ts
@@ -2,16 +2,27 @@ import { NextRequest, NextResponse } from "next/server";
 import fs from 'fs';
 import path from 'path';
 import Client from '@amazonpay/amazon-pay-api-sdk-nodejs';
+import { formatError } from "@/lib/utils";
+import { SecretsManager } from 'aws-sdk';
+import { get } from "http";
+const secretsManager = new SecretsManager();
 
 export async function POST(req: NextRequest): Promise<NextResponse> {
-    const keyPath = path.join(process.cwd(), 'tst', 'private.pem');
-    const privateKey = fs.readFileSync(keyPath, 'utf8');
-    console.log("Private Key: ", privateKey);
-    console.log("Key Path: ", keyPath);
 
     const payload = await req.json();
+    let privateKey = null;
 
-    console.log("Payload: ", payload);
+    try {
+        const data = await secretsManager.getSecretValue({ SecretId: "apayPrivateKey" }).promise();
+        if (data.SecretString) {
+            privateKey = data.SecretString;  // The private key in PEM format
+        } else {
+            privateKey = getPrivateKeyFromFile();
+        }
+    } catch (error) {
+        console.warn("Error reading private key file: ", formatError(error));
+        privateKey = getPrivateKeyFromFile();
+    }
 
     const config = {
         publicKeyId: process.env.NEXT_PUBLIC_AMAZON_PUBLIC_KEY_ID,
@@ -20,8 +31,6 @@ export async function POST(req: NextRequest): Promise<NextResponse> {
         sandbox: true,
         algorithm: 'AMZN-PAY-RSASSA-PSS-V2'
     };
-
-    console.log("Config: ", config);
 
     const testPayClient = new Client.AmazonPayClient(config);
 
@@ -32,4 +41,10 @@ export async function POST(req: NextRequest): Promise<NextResponse> {
     return NextResponse.json({
         signature: signature
     });
+}
+
+function getPrivateKeyFromFile() {
+    console.log("Falling back to local private key file.");
+    const keyPath = path.join(process.cwd(), 'tst', 'private.pem');
+    return fs.readFileSync(keyPath, 'utf8');
 }
