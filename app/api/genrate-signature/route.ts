0aaac55778530942bd19a913b0032a91d25ce61e
Shivam Sinha <shvmsnha@amazon.com>
2025-04-08 14:06:33 +0530

    AmazonPayButton debug logs

--- app/api/genrate-signature/route.ts
+++ app/api/genrate-signature/route.ts
@@ -6,8 +6,12 @@ import Client from '@amazonpay/amazon-pay-api-sdk-nodejs';
 export async function POST(req: NextRequest): Promise<NextResponse> {
     const keyPath = path.join(process.cwd(), 'tst', 'private.pem');
     const privateKey = fs.readFileSync(keyPath, 'utf8');
+    console.log("Private Key: ", privateKey);
+    console.log("Key Path: ", keyPath);
 
-    const payload = await req.json()
+    const payload = await req.json();
+
+    console.log("Payload: ", payload);
 
     const config = {
         publicKeyId: process.env.NEXT_PUBLIC_AMAZON_PUBLIC_KEY_ID,
@@ -16,6 +20,8 @@ export async function POST(req: NextRequest): Promise<NextResponse> {
         sandbox: true,
         algorithm: 'AMZN-PAY-RSASSA-PSS-V2'
     };
+
+    console.log("Config: ", config);
 
     const testPayClient = new Client.AmazonPayClient(config);
 
