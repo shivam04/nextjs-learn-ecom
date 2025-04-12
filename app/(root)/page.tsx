bf05c9b3e98c5864284d17fdcfece8a8d7e04dfd
Shivam Sinha <sinhashivam04@gmail.com>
2025-04-12 18:18:49 +0530

    Icon Boxes

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,3 +1,4 @@
+import IconBoxes from "@/components/icon-boxes";
 import ProductCarousel from "@/components/shared/product/product-carousel";
 import ProductList from "@/components/shared/product/product-list";
 import ViewAllProductsButton from "@/components/view-all-products-button";
@@ -15,6 +16,7 @@ const HomePage = async () => {
       { featuredProducts.length > 0 && <ProductCarousel data={featuredProducts} /> }
       <ProductList data={latestProducts} title="Newest Arrivals"/>
       <ViewAllProductsButton />
+      <IconBoxes />
     </>
   )
 }
