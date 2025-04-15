879a8fb1e2f039ddf6d9d7748ff7493c58f2a98b
Shivam Sinha <sinhashivam04@gmail.com>
2025-04-15 23:06:35 +0530

    jsonly

--- app/(root)/order/[id]/amazon-pay-button.tsx
+++ app/(root)/order/[id]/amazon-pay-button.tsx
@@ -5,6 +5,8 @@ import { Order } from "@/types";
 import Script from "next/script";
 
 const AmazonPayButton = ({ order }:{ order: Omit<Order, 'paymentResult'> }) => {
+    const showJSButton = process.env.NEXT_PUBLIC_AMAZON_CHECKOUT_BUTTON === "js";
+    console.log(showJSButton);
     const loadApayButton = () => {
         if (typeof window !== "undefined" && (window as any).amazon) {
             const payLoad = {
@@ -43,12 +45,135 @@ const AmazonPayButton = ({ order }:{ order: Omit<Order, 'paymentResult'> }) => {
             });
         }
     };
+
+
+    const loadApayJSButton = () => {
+        if (typeof window !== "undefined" && (window as any).amazon) {
+            (window as any).amazon.Pay.renderJSButton('#AmazonPayButton', {
+                merchantId: process.env.NEXT_PUBLIC_AMAZON_MERCHANT_ID,
+                ledgerCurrency: 'USD',
+                checkoutLanguage: 'en_US',
+                productType: 'PayAndShip',
+                placement: 'Cart',
+                buttonColor: 'Gold',
+                sandbox: true,
+                checkoutSessionConfig: {
+                    "storeId": process.env.NEXT_PUBLIC_AMAZON_STORE_ID,
+                    "paymentDetails": {
+                        "paymentIntent": "AuthorizeWithCapture"
+                    },
+                },
+                onInitCheckout: function (event: any) {
+                    console.log("oninitcheckout")
+                    console.log(event);
+                    return {
+                        "totalShippingAmount": {
+                            "amount": "0.00",
+                            "currencyCode": "USD"
+                        },
+                        "totalBaseAmount": {
+                            "amount": "500.44",
+                            "currencyCode": "USD"
+                        },
+                        "totalTaxAmount": {
+                            "amount": "0.00",
+                            "currencyCode": "USD"
+                        },
+                        "totalDiscountAmount": {
+                            "amount": "400.44",
+                            "currencyCode": "USD"
+                        },
+                        "totalChargeAmount": {
+                            "amount": "100.00",
+                            "currencyCode": "USD"
+                        },
+                        "deliveryOptions": [{
+                            "id": "ups_shipping-02-25.11",
+                            "price": {
+                                "amount": "500",
+                                "currencyCode": "USD"
+                            },
+                            "shippingMethod": {
+                                "shippingMethodName": "shipping-method-name-onInitCheckout",
+                                "shippingMethodCode": "shipping-method-code-onInitCheckout"
+                            },
+                            "shippingEstimate": [{
+                                "timeUnit": "HOUR",
+                                "value": 2
+                            }],
+                            "isDefault": true
+                        }]
+                    };
+                },
+                onShippingAddressSelection: function (event: any) {
+                    console.log(event);
+                    return {
+                        "totalShippingAmount": {
+                            "amount": "0.00",
+                            "currencyCode": "USD"
+                        },
+                        "totalBaseAmount": {
+                            "amount": "5.44",
+                            "currencyCode": "USD"
+                        },
+                        "totalTaxAmount": {
+                            "amount": "0.00",
+                            "currencyCode": "USD"
+                        },
+                        "totalDiscountAmount": {
+                            "amount": "4.44",
+                            "currencyCode": "USD"
+                        },
+                        "totalChargeAmount": {
+                            "amount": "1.00",
+                            "currencyCode": "USD"
+                        }
+                    };
+                },
+                onCompleteCheckout: function (event: any) {
+                    console.log(event);
+                },
+                onDeliveryOptionSelection: function (event: any) {
+                    console.log(event);
+                    return {
+                        "totalShippingAmount": {
+                            "amount": "0.00",
+                            "currencyCode": "USD"
+                        },
+                        "totalBaseAmount": {
+                            "amount": "5.44",
+                            "currencyCode": "USD"
+                        },
+                        "totalTaxAmount": {
+                            "amount": "0.00",
+                            "currencyCode": "USD"
+                        },
+                        "totalDiscountAmount": {
+                            "amount": "4.44",
+                            "currencyCode": "USD"
+                        },
+                        "totalChargeAmount": {
+                            "amount": "1.00",
+                            "currencyCode": "USD"
+                        }
+                    };
+                },
+                onCancel: function (event: any) {
+                    console.log(event);
+                },
+                onError: function (event: any) {
+                    console.log(event);
+                }
+            })
+        }
+    }
+
     return (
         <>
             <Script
               src="https://static-na.payments-amazon.com/checkout.js"
               strategy="afterInteractive" // Load after the page becomes interactive
-              onLoad={loadApayButton}
+              onLoad={loadApayJSButton}
             />
             <div id="AmazonPayButton"></div>
         </>
